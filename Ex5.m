4b6a2bf099e9854594398fc3793909a99e5bf1df
Chaverondier Thomas <tchaverond@bs406-s16-14.insa-lyon.fr>
2015-01-14 19:03:26 +0100

    Carried on Ex5.m
    InterX.m is to get intersection points between curves

--- Ex5.m
+++ Ex5.m
@@ -1,22 +1,76 @@
-r=2;
+R=2;
 K=100;
+
+
 % question 1
 figure  % figure 1
 hold on
 tic
 for i=0:12,
-    [T,Y] = ode45(@(t,y) grasshopper_grow(t,y,r,K),[0 10],[i*10]);
+    [T,Y] = ode45(@(t,y) grasshopper_grow(t,y,R,K),[0 5],[i*10]);
     plot(T,Y)
 end
 toc
+
 % question 2
 figure  % figure 2
 hold on
-for b=1:5
-    for a=1:5
-        fplot(@(x) (5*b*x^2)/(5*a^2+x^2),[0,50]);
+for B=1:5
+    for A=1:5
+        fplot(@(x) (5*B*x^2)/(5*A^2+x^2),[0,50]);
     end
 end
+
 % question 5
-% TODO : plot C and P to get intersection points, with different r and k
-% values
+% include alpha and lambda if switching back to effective population
+r=A*R/B;
+k=K/A;
+figure  % figure 3
+hold on
+x = 0:25;
+y1 = (x.*x)./(1+x.*x);
+plot(x,y1,'-g')
+k=20;
+for r=0.1:0.1:1   
+    y2 = r.*x.*(1-(x./k));
+    P = InterX([x;y1],[x;y2]);
+    plot(x,y2,P(1,:),P(2,:),'ro')
+end
+ylim([-3 5]);
+figure  % figure 4
+hold on
+x = 0:30;
+y1 = (x.*x)./(1+x.*x);
+plot(x,y1,'-g')
+r=5;
+for k=5:5:25
+    y2 = r.*x.*(1-(x./k));
+    P = InterX([x;y1],[x;y2]);
+    plot(x,y2,P(1,:),P(2,:),'ro')
+end
+ylim([-10 32])
+%TODO : get critical values of r and k !
+
+
+% question 6
+%TODO : plot surface f(x,k,r)=0 with x,k and r coordinates ?
+figure
+hold on
+x = 0:100;
+y1 = x.*0;
+tic
+for r=1:0.1:5
+    for k=1:0.1:4
+        y2 = r.*x.*(1-(x./k)) - ((x.*x)./(1+x.*x));
+        P = InterX([x;y1],[x;y2]);
+        P(:,1) = [];          % we don't care about the intersection (0;0)
+        R = r*ones(length(P(1,:)),1);
+        K = k*ones(length(P(1,:)),1);
+        plot3(P(1,:),R,K,'rx');
+    end
+end
+xlabel('x');
+ylabel('r');
+zlabel('k');
+toc
+
